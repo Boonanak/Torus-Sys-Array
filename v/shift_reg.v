977c09c3513b6be917cf7603d90cec9da0be8a55
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-03 10:32:04 -0800

    created shift register

---
+++ v/shift_reg.v
@@ -0,0 +1,54 @@
+// Serial in parallel out shift register
+// Parametrized depth and bit width
+// Supports write and read operations with full and empty flags
+module shift_reg #(  parameter WIDTH_p = 8,
+                     parameter DEPTH_p = 8
+                ) (
+                     input logic clk_i,
+                     input logic rst_n_i,
+                     input logic write_i,
+                     input logic read_i,
+                     input logic [WIDTH_p-1:0] data_i,
+                     output logic [WIDTH_p-1:0] [DEPTH_p-1:0] data_o,
+                     output logic full_o,
+                     output logic empty_o
+                  ); 
+
+   logic [$clog2(DEPTH_p) + 1:0] write_cnt; // Store how many elements have been written since last read
+   logic full, empty;
+   logic [WIDTH_p-1:0] [DEPTH_p-1:0] shift_reg; // Shift register bus
+
+   genvar i;
+   generate
+      // Set up each dff to send its content to the next dff
+      for (i = 0; i < DEPTH_p; i++) begin : shift_reg_gen
+         dff_en #(.WIDTH_p(WIDTH_p)) dff (
+            .clk_i(clk_i),
+            .rst_n_i(rst_n_i),
+            .en_i((write_i && !full)), // Enable if writing to this stage or if reading from the first stage
+            .data_i(i == 0 ? data_i : shift_reg[i-1]), // Input is either the new data or the previous stage's output
+            .data_o(shift_reg[i]) // Output goes to the shift register bus
+         );
+      end
+   endgenerate
+
+   // Write pointer logic
+   always_ff @(posedge clk_i) begin
+      if (!rst_n_i)
+         write_cnt <= 0;
+      else if (write_i && !full)
+         write_cnt <= write_cnt + 1; // Increment write counter on write
+      else if (read_i && !empty)
+         write_cnt <= 0; // Reset write counter on read
+      full_o <= full; // Update full output
+      empty_o <= empty; // Update empty output
+   end
+
+   // Full and empty flag logic
+   always_comb begin
+      full = (write_cnt == DEPTH_p); // Full when write counter reaches DEPTH_p (bitwidth set to fit decimal DEPTH_p)
+      empty = (write_cnt == 0); // Empty when write counter is at 0
+      data_o = shift_reg; // Output the current state of the shift register (combinational so we don't create more dffs)
+   end
+
+endmodule
