6dfaa768b4664233ee4bdade81a0b906fae021dd
Sachman360 <sachalpshaikh@gmail.com>
2026-03-05 15:52:16 -0800

    PE_final_trace.tr

--- scripts/trace_writer.py
+++ scripts/trace_writer.py
@@ -12,6 +12,8 @@ def write_trace(input_file_name, trace_file_name):
                 match tb_type:
                     case "PE":
                         trace.write(parse_PE_line(line))
+                    case "PE_final":
+                        trace.write(parse_PE_final_line(line))
                     case "DFF":
                         trace.write(parse_DFF_line(line))
                     case "SR":
@@ -57,6 +59,54 @@ def parse_PE_line(PE_line):
                 trace_line += f"0000__{'0'*25}\n"
         case 'end':
             trace_line += f"# ENDING SIMULATION\n0100__{'0'*25}\n"
+        case '###':
+            trace_line += PE_line
+    return trace_line + '\n'
+
+def parse_PE_final_line(PE_line):
+    space_i = PE_line.find(' ')
+    command = PE_line[:space_i] if space_i > 0 else PE_line
+    trace_line = ''
+    match command.casefold():
+        case 'load':
+            PE_line = PE_line[space_i+1:]
+            space_i = PE_line.find(' ')
+            major = True if PE_line[:space_i] == 'R' else False
+            B_in = int(PE_line[space_i:])
+            B_in_trace = f"00000000_{to_signed_nbit_binary(B_in, 8)}"
+            other_trace = '00000000_00000000'
+            trace_line += f"# SEND  |  load_B  |  {'row major' if major else 'col major'}  |  B = {B_in}\n"
+            trace_line += f"0001______1______{int(major)}______{B_in_trace if major else other_trace}______{other_trace if major else B_in_trace}\n"
+        case 'compute':
+            PE_line = PE_line[space_i+1:]
+            space_i = PE_line.find(' ')
+            major = True if PE_line[:space_i] == 'R' else False
+            numbers = [int(n) for n in PE_line[space_i:].split()]
+            A_in = numbers[0]
+            A_in_trace = f"00000000_{to_signed_nbit_binary(A_in, 8)}"
+            PS_in = numbers[1]
+            PS_in_trace = f"{to_signed_nbit_binary(PS_in, 16)}"
+            trace_line += f"# SEND  |  compute |  {'row major' if major else 'col major'}  |  A = {A_in}  |  PS = {PS_in}\n"
+            trace_line += f"0001______0______{int(major)}______{A_in_trace if major else PS_in_trace}______{PS_in_trace if major else A_in_trace}\n"
+        case 'recv':
+            PE_line = PE_line[space_i+1:]
+            print(PE_line)
+            space_i = PE_line.find(' ')
+            major = True if PE_line[:space_i] == 'R' else False
+            numbers = [int(n) for n in PE_line[space_i:].split()]
+            A_out = numbers[0]
+            A_out_trace = f"00000000_{to_signed_nbit_binary(A_out, 8)}"
+            PS_out = numbers[1]
+            PS_out_trace = f"{to_signed_nbit_binary(PS_out, 16)}"
+            trace_line += f"# RECV  |  A = {A_out}  |  PS = {PS_out}\n"
+            trace_line += f"0010______0______0______{A_out_trace if major else PS_out_trace}______{PS_out_trace if major else A_out_trace}\n"
+        case 'wait':
+            n = int(PE_line[space_i:])
+            trace_line += f"# WAIT for {n} cycles\n"
+            for i in range(n):
+                trace_line += f"0000__{'0'*34}\n"
+        case 'end':
+            trace_line += f"# ENDING SIMULATION\n0100__{'0'*34}\n"
         case '###':
             trace_line += PE_line
     return trace_line + '\n'
@@ -186,4 +236,4 @@ def to_signed_nbit_binary(integer, n_bits):
 
 
 
-write_trace('scripts/SR_test.txt', 'v/transpose/shift_reg_trace.tr')
+write_trace('scripts/PE_final_test.txt', 'v/transpose/PE_final_trace.tr')
