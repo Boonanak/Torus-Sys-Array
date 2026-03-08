90f3972fa59b5d3bfcdf93439d661ce3733b1244
Sachman360 <sachalpshaikh@gmail.com>
2026-03-08 15:11:41 -0700

    311 transpose

--- v/transpose/transpose.v
+++ v/transpose/transpose.v
@@ -136,7 +136,7 @@ module transpose #( parameter DIM_p = 8, // Dimensions of the matrix (DIM_p x DI
     // if direction is 0, we are shifting left, so the output 
     // data is in the last column of the bus
     generate
-        for (int i = 0; i < DIM_p; i++) begin
+        for (i = 0; i < DIM_p; i++) begin
             assign out_data[i] = direction ? tp_bus[DIM_p-1][i] : tp_bus[i][DIM_p-1];  
         end
     endgenerate
