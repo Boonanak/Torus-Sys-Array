fa85c8b8b3e743bf4ebc0bd8a5cd1d5c12c50aee
Sachman360 <sachalpshaikh@gmail.com>
2026-03-08 15:26:02 -0700

    325 tp_node_tb

--- v/transpose/tp_node_tb.v
+++ v/transpose/tp_node_tb.v
@@ -72,15 +72,15 @@ module tp_node_tb;
 
   logic row = tr_data_lo[42:40];
   logic col = tr_data_lo[39:37];
-  logic [7:0] data_pass_0_i  [0:DIM_p-1][0:DIM_p-1];
+  wire [7:0] data_pass_0_i  [0:DIM_p-1][0:DIM_p-1];
   assign data_pass_0_i[row][col] = tr_data_lo[31:24];
-  logic [7:0] data_pass_1_i  [0:DIM_p-1][0:DIM_p-1];
+  wire [7:0] data_pass_1_i  [0:DIM_p-1][0:DIM_p-1];
   assign data_pass_1_i[row][col] = tr_data_lo[23:16];
-  logic [7:0] data_shift_0_i [0:DIM_p-1][0:DIM_p-1];
+  wire [7:0] data_shift_0_i [0:DIM_p-1][0:DIM_p-1];
   assign data_shift_0_i[row][col] = tr_data_lo[15:8];
-  logic [7:0] data_shift_1_i [0:DIM_p-1][0:DIM_p-1];
+  wire [7:0] data_shift_1_i [0:DIM_p-1][0:DIM_p-1];
   assign data_shift_1_i[row][col] = tr_data_lo[7:0];
-  logic [7:0] data_out       [0:DIM_p-1][0:DIM_p-1];
+  wire [7:0] data_out       [0:DIM_p-1][0:DIM_p-1];
   assign dut_data_lo = {35'b0, data_out[row][col]};
 
   genvar r, c;
