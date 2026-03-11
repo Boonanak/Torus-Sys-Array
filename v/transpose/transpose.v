5834714c360aa510fc0c740b2ed58e12a9cd1541
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-10 17:54:24 -0700

    set up row and column selection bit logic, still needs a lot of work on ready-valid handshake

--- v/transpose/transpose.v
+++ v/transpose/transpose.v
@@ -16,18 +16,19 @@ module transpose #( parameter DIM_p = 8, // Dimensions of the matrix (DIM_p x DI
                     output logic [WIDTH_p-1:0] out_data [DIM_p-1:0] // full column output data
                   );
 
-    logic [DIM_CLOG2_p:0] state_counter; // Top bit is direction bit, bottom bits are the count 
     logic direction;
     logic [DIM_CLOG2_p-1:0] count;
+    logic [DIM_CLOG2_p:0] write_counter;
 
     logic [1:0] row_enable [DIM_p-1:0]; // 2 bit code for row shifting/passing
     logic [1:0] col_enable [DIM_p-1:0]; // 2 bit code for column shifting/passing
 
     logic [DIM_p-1:0] valid; // which row or column is valid. Shared based on direction
-    logic output_valid; // Top bit of the valid signal indicates if output is valid
+    logic output_valid; // Top bit of the valid signal indicates if output is valid, all valid is if every bit is valid
+    logic shift, enable;
 
     // States for ready-valid logic
-    enum logic [1:0] {EMPTY, FILL, FULL, DRAIN} curr, next;
+    enum logic [1:0] {EMPTY, PARTIAL, FULL} curr, next;
     // EMPTY: no values are in the transposer
     // FILL: We are putting values in but the output is not valid yet
     // FULL: transposer is full, output is valid. we can maintain this if we read and write at the same time
@@ -79,59 +80,59 @@ module transpose #( parameter DIM_p = 8, // Dimensions of the matrix (DIM_p x DI
         valid_tracker (
                        .clk_i(clk_i),
                        .rst_n_i(rst_n_i),  
-                       .shift_i(), 
-                       .data_in_i(), 
+                       .enable_i(shift), 
+                       .shift_in_i(valid_i), 
                        .data_out_o(valid) 
                       );
 
     // Ready-valid handshake logic based on current state
     always_ff @(posedge clk_i) begin
         if (~rst_n_i) begin
-            state_counter <= '0;
             ready_o <= 1'b1;
             valid_o <= 1'b0;
+            shift <= 1'b0;
             curr <= EMPTY;
+            enable <= 1'b0;
+            write_counter <= '0;
         end else begin
             curr <= next;
+            valid_o <= output_valid;
             if (curr == EMPTY) begin
                 ready_o <= 1'b1;
                 valid_o <= 1'b0;
-                // hold state counter
-            end else if (curr == FILL) begin
-                ready_o <= 1'b1;
-                valid_o <= 1'b0;
-                if (valid_i) begin
-                    state_counter <= state_counter + 1'b1; // Increment count if we have valid input data
-                end
+                shift <= 1'b0;
+                enable <= 1'b0;
+            end else if (curr == PARTIAL) begin
+                
             end else if (curr == FULL) begin
-                valid_o <= 1'b1;
-                if (ready_i) begin
-                    state_counter <= state_counter + 1'b1; // Increment count if we have valid input data
-                    ready_o <= 1'b1; // We can accept new data if old data is being read out
-                end else begin
-                    ready_o <= 1'b0; // cannot accept new data until old is read out 
-                end
-            end else if (curr == DRAIN) begin
-                ready_o <= 1'b0; // Not ready to accept new data when draining
-                valid_o <= 1'b1; // Output is still valid when draining
-                if (ready_i) begin
-                    state_counter <= state_counter + 1'b1;
-                end 
-            end 
+                
+            end     
         end
     end
 
     // Next-state logic 
-    // NOTE: Potential for glitches if valid_i glitches, we could go to the wrong state. 
-    // Mostly only a problem for FULL -> DRAIN which would cause lost throughput
     always_comb begin
         case(curr)
-            EMPTY: next = valid_i ? FILL : EMPTY; // If we have valid input data, start filling the pipeline
-            FILL: next = (count == DIM_p-1) ? FULL : FILL; // If we've filled the pipeline, go to full, else keep filling
-            FULL: next = valid_i ? FULL : DRAIN; // If we have more valid input data, stay full and keep accepting, else go to drain
-            DRAIN: next = (count == 0) ? EMPTY : DRAIN; // If we've drained the pipeline, go to empty, else keep draining
+            EMPTY: next = valid_i ? PARTIAL : EMPTY;
+            PARTIAL: next = (valid == 0) EMPTY : (valid == (DIM_p-1)) ? FULL : PARTIAL;
+            FULL: next = (valid == (DIM_p-1)) ? FULL : PARTIAL;
         endcase
     end
+
+    // Set the row and column enable lines
+    generate
+        logic selection;
+        for (int i = 0; i < DIM_p; i++) begin : enable_loop
+            if (i == 0) 
+                assign selection = 1'b0; // pass
+            else if (i < count)
+                assign selection = 1'b1; // shift
+            else
+                assign selection = 1'b0; // pass
+            assign col_enable[i] = direction ? '0 : {enable, selection};
+            assign row_enable[i] = direction ? {enable, selection} : '0;
+        end
+    endgenerate
 
     // if direction is 1, we are shifting up, 
     // so the output data is in the last row of the bus. 
@@ -144,9 +145,9 @@ module transpose #( parameter DIM_p = 8, // Dimensions of the matrix (DIM_p x DI
     endgenerate
 
     // Constant assignments for control signals
-    assign direction = state_counter[DIM_CLOG2_p]; // top bit of the counter is the direction bit
-    assign count = state_counter[DIM_CLOG2_p-1:0]; // bottom bits of the counter are the count
     assign output_valid = valid[DIM_p-1]; // The last bit of the valid shift register indicates if the output data is valid
+    assign direction = write_counter[DIM_CLOG2_p];
+    assign count = write_counter[DIM_CLOG2_p-1:0];
 
     // Assertions to check for valid parameter settings
     initial begin
@@ -162,24 +163,9 @@ module transpose #( parameter DIM_p = 8, // Dimensions of the matrix (DIM_p x DI
 
 endmodule
 
-
 /*
-If the transposer it empty, output ready_o and ~valid_o, and wait for valid_i
-on valid_i, enable the array, and shift in the data.
-If valid_i remains true, keep shifting, else wait for valid_i. Maintain ready_o
+    direction will flip every DIM_p cycles if the last row/col is valid
 
-once data reaches the end of the pipeline, set valid_o and freeze the pipeline
-if ready_i is set, advance the pipeline until it goes false
-if there is valid data to put into the pipeline that can immediatly follow the previous, keep accpeting new data
-if not, set ~ready_o and wait to empty the pipeline
-once empty, reset, ready_o
-============================================
-if row = 0 pass1 = input data
-if col = 0 pass0 = input data
+    row = 0 and col = 0 will always be pass
 
-if col > 0 shift0 = bus[col-1][row-1] unless row-1 = -1, then shift0 = bus[col-1][DIM_p-1]
-if col > 0 pass0 = bus[col-1][row]
-
-if row > 0 shift1 = bus[col-1][row-1] unless col-1 = -1, then shift1 = bus[DIM_p-1][row-1]
-if row > 0 pass1 = bus[col][row-1]
 */
