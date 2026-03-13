058e629dc33bf1541982d33c4cbc8c31943e7bd1
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-12 18:12:12 -0700

    changed default params to trick synth tool into leaving them

--- v/transpose/transpose.sv
+++ v/transpose/transpose.sv
@@ -1,7 +1,7 @@
 // A pipelined transpose module for a DIM_p x DIM_p matrix with WIDTH_p bit elements
 // Takes in a full row of an input matrix and outputs a full column of the transposed matrix
-module transpose #( parameter DIM_p = 8, // Dimensions of the matrix (DIM_p x DIM_p) (MUST BE POWER OF 2)
-                    parameter WIDTH_p = 8) // Width of data
+module transpose #( parameter DIM_p = -1, // Dimensions of the matrix (DIM_p x DIM_p) (MUST BE POWER OF 2)
+                    parameter WIDTH_p = -1) // Width of data
                   ( input logic clk_i, 
                     input logic rst_n_i, // Active low reset
                     input logic col_major_i, // input is in column-major order (NONFUNCTIONAL)
