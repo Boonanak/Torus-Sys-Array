c97e687b92be030f0ec1b6bb0abab302a09db9de
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-13 12:03:08 -0700

    should pass when not rotating

--- v/transpose/transpose.sv
+++ v/transpose/transpose.sv
@@ -170,7 +170,7 @@ module transpose #( parameter DIM_p = 4, // Dimensions of the matrix (DIM_p x DI
     generate 
         for (j = 0; j < DIM_p; j++) begin : selection_loop
             // first line always passes, then we shift more and more lines as count increases, then we go back to passing after count exceeds the index
-            assign selection[j] = (j == 0 || rotate) ? PASS : (j <= count) ? SHIFT : PASS; 
+            assign selection[j] = (j == 0 || ~rotate) ? PASS : (j <= count) ? SHIFT : PASS; 
         end
     endgenerate
 
