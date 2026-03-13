9912fdbef2aa10d5cd4e88c5c78e2f250fa33ee7
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-13 15:38:26 -0700

    reversed - swap first

--- v/transpose/transpose.sv
+++ v/transpose/transpose.sv
@@ -117,8 +117,8 @@ module transpose #( parameter DIM_p = 4, // Dimensions of the matrix (DIM_p x DI
                 //if col > 0 pass0 = bus[row][col-1]
                 //if row > 0 pass1 = bus[row-1][col]
 
-                assign data_pass_1[row][col] = (row == 0) ? (processed_in_data[col]) : tp_bus[row-1][col];
-                assign data_pass_0[row][col] = (col == 0) ? (processed_in_data[row]) : tp_bus[row][col-1];
+                assign data_pass_1[row][col] = (row == 0) ? (flipped_in_data[col]) : tp_bus[row-1][col];
+                assign data_pass_0[row][col] = (col == 0) ? (flipped_in_data[row]) : tp_bus[row][col-1];
 
                 assign data_shift_0[row][col] = (col == 0) ? 'X : tp_bus[(row - shift_amount_row + DIM_p) % DIM_p][col - 1];
                 assign data_shift_1[row][col] = (row == 0) ? 'X : tp_bus[row - 1][(col - shift_amount_col + DIM_p) % DIM_p];
