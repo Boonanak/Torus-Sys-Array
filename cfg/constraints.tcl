fb4e940fa830a66057728a993b8444d35d0b66a8
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-13 18:36:39 -0700

    added proper constraints for feedthrough paths

--- cfg/constraints.tcl
+++ cfg/constraints.tcl
@@ -5,13 +5,23 @@
 # you may manually define constraints here as well.
 #
 
-create_clock -name clk -period 12 [get_ports clk_i]
+# Defined Variables
+set clk_period 12
+set half_period [expr {$clk_period / 2}]
+
+# List of feedthough I/O to exclude
+set feedthrough_inputs [list transpose ready_i]
+set feedthrough_outputs [list ready_o out_data[*][*]]
+
+create_clock -name clk -period $clk_period [get_ports clk_i]
 set_clock_uncertainty 0.050 [get_clocks clk]
 
 # Always set the input/output delay as half periods for clock setup checks
-set_input_delay  6 -max -clock [get_clocks clk] [all_inputs]
-set_output_delay 6 -max -clock [get_clocks clk] [remove_from_collection [all_outputs] [get_ports clk_o]]
+set_input_delay $half_period -max -clock [get_clocks clk] [remove_from_collection [all_inputs] [get_ports $feedthrough_inputs]]
+set_output_delay $half_period -max -clock [get_clocks clk] [remove_from_collection [remove_from_collection [all_outputs] [get_ports clk_o]] [get_ports $feedthrough_outputs]]
 
-# Always set the input/output delay as 0 for clock hold checks
-set_input_delay  0.0 -min -clock [get_clocks clk] [all_inputs]
-set_output_delay 0.0 -min -clock [get_clocks clk] [remove_from_collection [all_outputs] [get_ports clk_o]]
+# Constrain feedthrough paths to take less time than max delay
+# Add more feedthrough paths as they appear
+# half period is very strict, usually at most 0.7 x period, but higher for safety
+set_max_delay $half_period -from [get_ports transpose] -to [get_ports {out_data[*][*]}]
+set_max_delay $half_period -from [get_ports ready_i] -to [get_ports ready_o]
