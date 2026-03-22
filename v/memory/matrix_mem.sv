f4d528e7ed3a235cbdd24b6679b87e2e95fdb975
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-21 17:18:27 -0700

    basic memory template for testing purposes

---
+++ v/memory/matrix_mem.sv
@@ -0,0 +1,28 @@
+// Implments a 1 read 1 write port memory with async read and sync write
+// Designed to store vectors for sets of matricies
+module matrix_mem #( parameter WIDTH_p, // Width of the individual vectors
+                     parameter DIM_p,   // Grouping of vectors (how many vectors make up a single matrix)
+                     parameter ENTRIES_p // Number of matrices
+                   ) (
+                     input logic clk_i,
+                     input logic reset_n_i,
+                     input logic wren,
+                     input logic  [ADDRESS_WIDTH_lp-1:0] rd_addr,
+                     input logic  [ADDRESS_WIDTH_lp-1:0] wr_addr, 
+                     input logic  [WIDTH_P-1:0] wr_data,
+                     output logic [WIDTH_P-1:0] rd_data
+                   );
+
+    localparam NUM_ADDRESSES_lp = DIM_p * ENTRIES_p; // Number of address in the memory
+    localparam ADDRESS_WIDTH_lp = $clog2(NUM_ADDRESSES_lp); // minimum bitwidth required to store NUM_ADDRESSES_lp
+    
+    logic [WIDTH_P-1:0] mem [NUM_ADDRESSES_lp-1:0]; // Memory Array
+
+    always_ff @(posedge clk_i) begin
+        if (wren)
+            mem[wr_addr] <= wr_data;
+    end
+
+    assign rd_data = mem[rd_addr];
+
+endmodule 
