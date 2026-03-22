ba840230c85b70a20498bf4df54d8354c8ea94be
SpanishMoss00 <spanishmoss00@gmail.com>
2026-03-21 17:26:30 -0700

    fixed header ,

--- v/memory/matrix_mem.sv
+++ v/memory/matrix_mem.sv
@@ -2,9 +2,9 @@
 // Designed to store vectors for sets of matricies
 module matrix_mem #( parameter WIDTH_p = 64, // Width of the individual vectors
                      parameter DIM_p = 8,   // Grouping of vectors (how many vectors make up a single matrix)
-                     parameter ENTRIES_p = 64 // Number of matrices
-                     localparam NUM_ADDRESSES_lp = DIM_p * ENTRIES_p; // Number of address in the memory
-                     localparam ADDRESS_WIDTH_lp = $clog2(NUM_ADDRESSES_lp); // minimum bitwidth required to store NUM_ADDRESSES_lp
+                     parameter ENTRIES_p = 64, // Number of matrices
+                     localparam NUM_ADDRESSES_lp = DIM_p * ENTRIES_p, // Number of address in the memory
+                     localparam ADDRESS_WIDTH_lp = $clog2(NUM_ADDRESSES_lp) // minimum bitwidth required to store NUM_ADDRESSES_lp
                    ) (
                      input logic clk_i,
                      input logic reset_n_i,
