10e126f92fb374ef491f2e172a82464c6ebc0c2e
hapuum <lbklbk9345@gmail.com>
2026-03-29 05:07:41 -0700

    improve timing by changing A/PS to be stored reverse instead of storing regularly and muxing with row major reg on output

--- v/PE/Pipette_PE.sv
+++ v/PE/Pipette_PE.sv
@@ -52,15 +52,15 @@ module Pipette_PE # (
         end
 
         overflow_safe_computation = A * B + PS;
-        // any way to write this parameterized and no magic number?
+        // TODO: any way to write this parameterized and no magic number?
         alu_result = (overflow_safe_computation < -32768) ? 16'h8000 :
                      (overflow_safe_computation > 32767) ? 16'h7FFF : 
                      overflow_safe_computation[PS_WIDTH - 1:0];
     end
 
     assign B_out = Potential_B_reg;
-    assign A_out = row_major_reg ? A_reg : PS_reg;
-    assign PS_out = row_major_reg ? PS_reg : A_reg;
+    assign A_out = A_reg;
+    assign PS_out = PS_reg;
 
     always_ff @(posedge clk_i) begin 
         if (reset) begin 
@@ -72,8 +72,10 @@ module Pipette_PE # (
             row_major_reg <= '0;
         end
         else begin 
-            A_reg <= A;
-            PS_reg <= enable_A ? alu_result : PS_reg;
+            // update A_reg and PS_reg on enable
+            // choose correct one to store based on row_major
+            A_reg <= enable_A ? (row_major ? A : alu_result) : A_reg;
+            PS_reg <= enable_A ? (row_major ? alu_result : A) : PS_reg;
             B_reg <= B;
             Potential_B_reg <= Potential_B;
             active_lock_pulse_out <= enable_B ? active_lock_pulse_in : active_lock_pulse_out;
