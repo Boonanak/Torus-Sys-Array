8ca9086515e365c7065166147c542346fb9bef92
Sean Bubernak <sean.bubernak@gmail.com>
2026-04-23 15:11:49 -0700

    Added packet_size_i input to handle when a line has zero flits that can be ignored

--- v/Top_level/depacketizer.sv
+++ v/Top_level/depacketizer.sv
@@ -1,10 +1,15 @@
 /*
 Inputs:
- clk_i: core clock
- reset_i: core reset
- packet_i (from mem): The complete line (or packet) from memory
- valid_i (from mem): Valid packet data
- ready_i (to upstream): bsg_link_upstream is ready for more data
+  clk_i: core clock
+  reset_i: core reset
+  packet_i (from mem): The complete line (or packet) from memory
+  valid_i (from mem): Valid packet data
+  ready_i (to upstream): bsg_link_upstream is ready for more data
+  packet_size_i (from mem): Which flits are actually valid in the 128 bit block
+    Size 1: Stop when cnt is 0 (Three bottom flits are 0)
+    Size 2: Stop when cnt is 1 (Two bottom flits are 0)
+    Size 3: Stop when cnt is 2 (One bottom flit is 0)
+    Size 4 (or 0): Stop when cnt is 3 (Full packet)
 Outputs:
   ready_o: (from mem): depacketizer is ready for more data
   flit_o (to upstream): Outputted flit to bsg_link_upstream
@@ -24,25 +29,26 @@ module depacketizer
   , parameter flit_width_p = 32
   , parameter fifo_els_p   = 4
 ) (
-  input                        clk_i
-  , input                      reset_i
+  input                                clk_i
+  , input                              reset_i
 
   // Input from Memory
-  , input [packet_width_p-1:0] packet_i
-  , input                      valid_i
-  , output logic               ready_o
+  , input [packet_width_p-1:0]         packet_i
+  , input                              valid_i
+  , input [1:0]                        packet_size_i
+  , output logic                       ready_o
 
   // Output to bsg_upstream
-  , output [flit_width_p-1:0]  flit_o
-  , output                     valid_o
-  , input                      ready_i
+  , output [flit_width_p-1:0]          flit_o
+  , output                             valid_o
+  , input                              ready_i
 );
 
   localparam num_flits_lp = packet_width_p / flit_width_p;
 
-  // How many bits the counter needs
   logic [$clog2(num_flits_lp)-1:0] flit_cnt_r, flit_cnt_n;
   logic [packet_width_p-1:0] packet_r;
+  logic [1:0] packet_size_r;
   logic packet_v_r, packet_v_n;
   
   logic fifo_ready_lo;
@@ -50,12 +56,9 @@ module depacketizer
   logic flit_valid_lo;
 
   // --- Control Logic ---
-  // We only accept a new packet when the current one is finished 
-  // and the FIFO has room to start the sequence.
-  // Is driven explicitly (not just passed in to something) which is why it has to be "logic"
-  assign ready_o = (flit_cnt_r == '0) && !packet_v_r;
+  assign ready_o = (packet_v_r == 1'b0);
 
-  always_comb begin
+always_comb begin
     flit_cnt_n    = flit_cnt_r;
     packet_v_n    = packet_v_r;
     flit_valid_lo = 1'b0;
@@ -64,10 +67,13 @@ module depacketizer
       flit_valid_lo = 1'b1;
       if (fifo_ready_lo) begin
         flit_cnt_n = flit_cnt_r + 1'b1;
-        // After 4th flit (11 -> 00), clear the current packet
-        if (flit_cnt_r == (num_flits_lp - 1)) begin
-          packet_v_n = 1'b0;
-        end
+
+        case (packet_size_i)
+          2'd1:    if (flit_cnt_r == 2'd0) packet_v_n = 1'b0;
+          2'd2:    if (flit_cnt_r == 2'd1) packet_v_n = 1'b0;
+          2'd3:    if (flit_cnt_r == 2'd2) packet_v_n = 1'b0;
+          default: if (flit_cnt_r == 2'd3) packet_v_n = 1'b0; // Full 4 flits
+        endcase
       end
     end else if (valid_i && ready_o) begin
       packet_v_n = 1'b1;
@@ -75,10 +81,7 @@ module depacketizer
     end
   end
 
-  // --- Big Endian Mux: 127:96, then 95:64, etc. ---
-  // Using flit_cnt_r to select the slice
-  // Ex: flit_cnt_r - 0 and flit_width_p = 32
-  // packet_r[(3 - 0) * 32 +: 32] = packet_r[96 +: 32] ... [127:96]
+  // --- Big Endian Mux ---
   assign flit_mux_lo = packet_r[((num_flits_lp - 1) - flit_cnt_r)*flit_width_p +: flit_width_p];
 
   always_ff @(posedge clk_i) begin
@@ -86,28 +89,27 @@ module depacketizer
       flit_cnt_r <= '0;
       packet_v_r <= 1'b0;
       packet_r   <= '0;
+      packet_size_r <= '0;
     end else begin
       flit_cnt_r <= flit_cnt_n;
       packet_v_r <= packet_v_n;
       if (ready_o && valid_i) begin
         packet_r <= packet_i;
+        packet_size_r <= packet_size_i;
       end
     end
   end
 
-  // --- Output Skid/Sync FIFO ---
-  // 4-element deep so it can hold 1 complete packet (line from memory) at a time
+  // --- Output FIFO ---
   bsg_fifo_1r1w_small #(
     .width_p(flit_width_p)
     ,.els_p(fifo_els_p)
   ) out_fifo (
-    .clk_i   (clk_i)
-    ,.reset_i (reset_i)
-
+    .clk_i    (clk_i)
+    ,.reset_i  (reset_i)
     ,.data_i  (flit_mux_lo)
     ,.v_i     (flit_valid_lo)
     ,.ready_o (fifo_ready_lo)
-
     ,.data_o  (flit_o)
     ,.v_o     (valid_o)
     ,.yumi_i  (valid_o & ready_i)
