aa820c32bf2e4f54ee50e25858502f6293c8d103
Allan <allanlyn.sea@outlook.com>
2026-04-26 19:35:46 -0700

    PE last modify

--- v/PE/PE_pkg.sv
+++ v/PE/PE_pkg.sv
@@ -3,5 +3,6 @@ package PE_pkg;
     // might refactor this package for easier int8/16 conversion for shared wires
     typedef logic signed [15:0] int16_t;
     typedef logic signed [7:0] int8_t;
+    typedef logic signed [18:0] int19_t;  // psum element width for N=8 mesh
 
 endpackage
