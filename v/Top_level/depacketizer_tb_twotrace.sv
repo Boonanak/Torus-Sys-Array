da552dbac076d8fa919352c1040a1f06edb9dc77
Sachman360 <sachalpshaikh@gmail.com>
2026-04-28 01:07:07 -0700

    lots of handshake changes based on claude's advice

--- v/Top_level/depacketizer_tb_twotrace.sv
+++ v/Top_level/depacketizer_tb_twotrace.sv
@@ -28,7 +28,9 @@ module depacketizer_tb_twotrace;
   logic [129:0] tr_data_lo;
   logic tr_ready_lo, tr_ready_r;
 
-  logic [31:0] rom_addr_li;
+  //logic [31:0] rom_addr_li;
+  logic [31:0] rom_addr_send_li; // this was an interesting note. Claude suggested separating these addresses. Let's see if it works
+  logic [31:0] rom_addr_recv_li; // new address for recv
   logic [133:0] rom_data_lo_send;
   logic [35:0]  rom_data_lo_recv;
 
@@ -37,7 +39,7 @@ module depacketizer_tb_twotrace;
     // / 4 bit trace command / 2 bit packet size / 128 bit packet /
     depacketizer_send_trace_rom #(.width_p(134),.addr_width_p(32))
     ROM_BPS_send
-        (.addr_i( rom_addr_li )
+        (.addr_i( rom_addr_send_li ) // send address
         ,.data_o( rom_data_lo_send )
         );
 
@@ -48,15 +50,15 @@ module depacketizer_tb_twotrace;
         , .reset_i( reset )
         , .en_i( 1'b1 )
 
-        , .v_i    ( dut_v_r )
-        , .data_i (  ) // nothing
+        , .v_i    ( 1'b0 ) // in theory, there should be no valid_i, since send side is not checking any inputs. previously dut_v_r, now 0
+        , .data_i ( 0 ) // nothing
         , .ready_o(  ) // nothing
 
         , .v_o   ( tr_v_lo )
         , .data_o( tr_data_lo )
-        , .yumi_i( tr_yumi_li )
+        , .yumi_i( tr_yumi_send ) // acknowledge send trace replay's output (DUT consumed the packet). previously gated dut_ready_r & tr_v_lo, now gated dut_ready_lo & tr_v_lo
 
-        , .rom_addr_o( rom_addr_li )
+        , .rom_addr_o( rom_addr_send_li ) // new send address
         , .rom_data_i( rom_data_lo_send )
 
         , .done_o()
@@ -66,7 +68,7 @@ module depacketizer_tb_twotrace;
     // / 4 bit trace command / 32 bit flit /    
     depacketizer_recv_trace_rom #(.width_p(36),.addr_width_p(32))
     ROM_BPS_recv
-        (.addr_i( rom_addr_li )
+        (.addr_i( rom_addr_recv_li ) // recv address
         ,.data_o( rom_data_lo_recv )
         );
 
@@ -83,20 +85,22 @@ module depacketizer_tb_twotrace;
 
         , .v_o   (  )
         , .data_o(  )
-        , .yumi_i( tr_yumi_li )
+        , .yumi_i( tr_yumi_recv ) // should be driven by DUT output (did DUT produce a flit that recv accepted). previously, gated dut_ready_r & tr_v_lo, now gated dut_v_lo & tr_ready_lo
 
-        , .rom_addr_o(  )
+        , .rom_addr_o( rom_addr_recv_li ) // new recv address
         , .rom_data_i( rom_data_lo_recv )
 
         , .done_o()
         , .error_o()
         );
 
-  always_ff @(negedge clk) begin
-    dut_ready_r <= dut_ready_lo;
-    tr_yumi_li  <= dut_ready_r & tr_v_lo;
-    dut_v_r     <= dut_v_lo;
-    dut_data_r  <= dut_data_lo;
+  always_ff @(posedge clk) begin // according to Claude, this negedge sequential block adds 1/2 cycle delay. let's try making it posedge?
+    dut_ready_r  <= dut_ready_lo;
+    //tr_yumi_li   <= dut_ready_r & tr_v_lo;
+    tr_yumi_send <= dut_ready_lo & tr_v_lo;
+    tr_yumi_recv <= dut_v_lo & tr_ready_lo;
+    dut_v_r      <= dut_v_lo;
+    dut_data_r   <= dut_data_lo;
   end
 
 
@@ -110,14 +114,14 @@ module depacketizer_tb_twotrace;
         .packet_i ( tr_data_lo[127:0] ),
         .valid_i ( tr_v_lo ),
         .packet_size_i ( tr_data_lo[129:128] ),
-        .ready_o ( dut_ready_lo ),
+        .ready_o ( dut_ready_lo ), // handshake r_o (input side) --> does it accept new packets from send?
         .flit_o ( dut_data_lo ),
         .valid_o ( dut_v_lo ),
-        .ready_i ( dut_v_lo ) // handshake r_i --> back to dut_v_lo i guess :(
+        .ready_i ( tr_ready_lo ) // handshake r_i --> dut_yumi_li? dut_v_lo (not real handshake)? Claude suggests tr_ready_lo, which hasn't worked well in the past but we will try it
     );
 
-  always_ff @(negedge clk) begin
-    dut_yumi_li <= tr_ready_lo & dut_v_lo;
+  always_ff @(posedge clk) begin // same thing here, posedge (likely should revert)
+    dut_yumi_li <= tr_ready_lo & dut_v_lo; // this is just tr_yumi_recv, Claude still suggests putting this into the ready_i port (as it originally was). Let's see if tr_ready_lo even works and then try that
   end
 
 endmodule
