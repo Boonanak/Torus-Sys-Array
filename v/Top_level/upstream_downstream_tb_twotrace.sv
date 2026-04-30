6ece38bb488ad1b3573f36a1421eafe72b9b8dd3
Sachman360 <sachalpshaikh@gmail.com>
2026-04-30 13:45:17 -0700

    more token clock pulses

--- v/Top_level/upstream_downstream_tb_twotrace.sv
+++ v/Top_level/upstream_downstream_tb_twotrace.sv
@@ -302,7 +302,7 @@ module upstream_downstream_tb_twotrace;
   logic parity_error;
 
   logic token_clk_init;
-  logic [3:0] token_init_count;
+  logic [4:0] token_init_count;
   logic token_init_done;
 
   // During reset sequence, pulse token_clk at io_master_clk rate
@@ -311,7 +311,7 @@ module upstream_downstream_tb_twotrace;
       if (tb_reset) begin
           token_init_count = 0;
           token_clk_init = 0;
-      end else if (!calib_done && token_init_count < 4'hF) begin
+      end else if (!calib_done && token_init_count < 8'hF0) begin
           token_init_count <= token_init_count + 1;
           token_clk_init   <= ~token_clk_init;
       end
