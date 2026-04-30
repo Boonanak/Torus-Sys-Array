5b159882835db4bab94e6959b75f38b3322c4bfe
Sachman360 <sachalpshaikh@gmail.com>
2026-04-30 01:37:28 -0700

    instant. name

--- v/Top_level/upstream_downstream_tb_twotrace.sv
+++ v/Top_level/upstream_downstream_tb_twotrace.sv
@@ -50,7 +50,7 @@ module upstream_downstream_tb_twotrace;
 
   logic downstream_reset;
   bsg_nonsynth_reset_gen #(.num_clocks_p(1),.reset_cycles_lo_p(4), .reset_cycles_hi_p(20))
-  reset_gen_4
+  reset_gen_5
     (.clk_i         ( clk )
     ,.async_reset_o ( token_reset )
     );
