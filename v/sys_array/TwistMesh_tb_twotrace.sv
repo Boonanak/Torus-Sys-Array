049430c3e8e36d126cd7c5945b088a31aad883d5
Sachman360 <sachalpshaikh@gmail.com>
2026-05-08 12:27:23 -0700

    moved files back to 32 bit, not fixed yet

--- v/sys_array/TwistMesh_tb_twotrace.sv
+++ v/sys_array/TwistMesh_tb_twotrace.sv
@@ -20,34 +20,49 @@ module TwistMesh_tb_twotrace;
       ,.async_reset_o( reset )
       );
 
+  // ================================================================
+  // Parameters
+  // ================================================================
+  localparam N = 8;
+  localparam IW = 8;
+  localparam WW = 8;
+  // localparam OW = 16;
+  localparam OW = 32;                                                        // T2SA-MESH: int32 psum
+  localparam OUTPUT_DATA_SIZE = N * OW;
+  localparam INPUT_DATA_SIZE = 1 + N + N*IW + N*WW + N*OW;
+  localparam IFMAP_START = INPUT_DATA_SIZE-2-N;
+  localparam WEIGHT_START = IFMAP_START-8*IW;
+  localparam PSUM_START = OUTPUT_DATA_SIZE - 1;
+
   logic dut_v_lo, dut_v_r;
-  logic [127:0] dut_data_lo, dut_data_r;
+  logic [OUTPUT_DATA_SIZE-1:0] dut_data_lo, dut_data_r;
   logic dut_ready_lo, dut_ready_r;
 
   logic tr_v_lo;
-  logic [264:0] tr_data_lo;
+  logic [INPUT_DATA_SIZE-1:0] tr_data_lo;
   logic tr_ready_lo, tr_ready_r;
 
   logic [31:0] rom_addr_li;
-  logic [269:0] rom_data_lo_send;
-  logic [131:0]  rom_data_lo_recv;
+  logic [INPUT_DATA_SIZE+3:0] rom_data_lo_send;
+  logic [OUTPUT_DATA_SIZE+3:0]  rom_data_lo_recv;
 
   logic tr_yumi_li, dut_yumi_li;
+  logic tr_yumi_send, tr_yumi_recv; // NEW
 
-  TwistMesh_send_trace_rom #(.width_p(269),.addr_width_p(32))
+  TwistMesh_send_trace_rom #(.width_p(INPUT_DATA_SIZE+4),.addr_width_p(32))
     ROM_BPS_send
       (.addr_i( rom_addr_li )
       ,.data_o( rom_data_lo_send )
       );
 
-  bsg_fsb_node_trace_replay #(.ring_width_p(265)
+  bsg_fsb_node_trace_replay #(.ring_width_p(INPUT_DATA_SIZE)
                              ,.rom_addr_width_p(32) )
     trace_replay_send
       ( .clk_i ( ~clk ) // Trace Replay should run on negative clock edge!
       , .reset_i( reset )
       , .en_i( 1'b1 )
 
-      , .v_i    ( dut_v_r )
+      , .v_i    (  )
       , .data_i (  )
       , .ready_o(  )
 
@@ -62,21 +77,21 @@ module TwistMesh_tb_twotrace;
       , .error_o()
       );
 
-  TwistMesh_recv_trace_rom #(.width_p(132),.addr_width_p(32))
+  TwistMesh_recv_trace_rom #(.width_p(OUTPUT_DATA_SIZE+4),.addr_width_p(32))
     ROM_BPS_recv
       (.addr_i( rom_addr_li )
       ,.data_o( rom_data_lo_recv )
       );
 
-  bsg_fsb_node_trace_replay #(.ring_width_p(128)
+  bsg_fsb_node_trace_replay #(.ring_width_p(OUTPUT_DATA_SIZE)
                              ,.rom_addr_width_p(32) )
     trace_replay_recv
       ( .clk_i ( ~clk ) // Trace Replay should run on negative clock edge!
       , .reset_i( reset )
       , .en_i( 1'b1 )
 
-      , .v_i    ( dut_v_r )
-      , .data_i ( dut_data_r )
+      , .v_i    ( dut_v_lo ) // NEW (no longer registered)
+      , .data_i ( dut_data_r ) // NEW (no longer registered)
       , .ready_o( tr_ready_lo )
 
       , .v_o   (  )
@@ -92,18 +107,13 @@ module TwistMesh_tb_twotrace;
 
   always_ff @(negedge clk) begin
     dut_ready_r <= dut_ready_lo;
-    tr_yumi_li  <= '1;
+    tr_yumi_li   <= dut_ready_r & tr_v_lo;
+    // tr_yumi_li  <= '1;
     dut_v_r     <= dut_v_lo;
     dut_data_r  <= dut_data_lo;
   end
 
-    // ================================================================
-    // Parameters
-    // ================================================================
-    localparam N = 8;
-    localparam IW = 8;
-    localparam WW = 8;
-    localparam OW = 16;
+
 
     // ================================================================
     // DUT signals
@@ -115,14 +125,14 @@ module TwistMesh_tb_twotrace;
     logic                  in_propagate;
     logic signed [OW-1:0]  out_psum   [N-1:0];
 
-    assign in_weight = {tr_data_lo[191:184], tr_data_lo[183:176], tr_data_lo[175:168], tr_data_lo[167:160],
-                        tr_data_lo[159:152], tr_data_lo[151:144], tr_data_lo[143:136], tr_data_lo[135:128]};
-    assign in_lock   = {>>{tr_data_lo[263:256]}};
-    assign in_ifmap  = {tr_data_lo[255:248], tr_data_lo[247:240], tr_data_lo[239:232], tr_data_lo[231:224],
-                        tr_data_lo[223:216], tr_data_lo[215:208], tr_data_lo[207:200], tr_data_lo[199:192]};
-    assign in_psum   = {tr_data_lo[127:112], tr_data_lo[111:96], tr_data_lo[95:80], tr_data_lo[79:64],
-                        tr_data_lo[63:48], tr_data_lo[47:32], tr_data_lo[31:16], tr_data_lo[15:0]};
-    assign in_propagate = tr_data_lo[264];
+    assign in_weight = {tr_data_lo[WEIGHT_START:WEIGHT_START-WW+1], tr_data_lo[WEIGHT_START-WW:WEIGHT_START-2*WW+1], tr_data_lo[WEIGHT_START-2*WW:WEIGHT_START-3*WW+1], tr_data_lo[WEIGHT_START-3*WW:WEIGHT_START-4*WW+1],
+                        tr_data_lo[WEIGHT_START-4*WW:WEIGHT_START-5*WW+1], tr_data_lo[WEIGHT_START-5*WW:WEIGHT_START-6*WW+1], tr_data_lo[WEIGHT_START-6*WW:WEIGHT_START-7*WW+1], tr_data_lo[WEIGHT_START-7*WW:WEIGHT_START-8*WW+1]};
+    assign in_lock   = {>>{tr_data_lo[INPUT_DATA_SIZE-2:INPUT_DATA_SIZE-1-N]}};
+    assign in_ifmap  = {tr_data_lo[IFMAP_START:IFMAP_START-IW+1], tr_data_lo[IFMAP_START-IW:IFMAP_START-2*IW+1], tr_data_lo[IFMAP_START-2*IW:IFMAP_START-3*IW+1], tr_data_lo[IFMAP_START-3*IW:IFMAP_START-4*IW+1],
+                        tr_data_lo[IFMAP_START-4*IW:IFMAP_START-5*IW+1], tr_data_lo[IFMAP_START-5*IW:IFMAP_START-6*IW+1], tr_data_lo[IFMAP_START-6*IW:IFMAP_START-7*IW+1], tr_data_lo[IFMAP_START-7*IW:IFMAP_START-8*IW+1]};
+    assign in_psum   = {tr_data_lo[PSUM_START:PSUM_START-OW+1], tr_data_lo[PSUM_START-OW:PSUM_START-2*OW+1], tr_data_lo[PSUM_START-2*OW:PSUM_START-3*OW+1], tr_data_lo[PSUM_START-3*OW:PSUM_START-4*OW+1],
+                        tr_data_lo[PSUM_START-4*OW:PSUM_START-5*OW+1], tr_data_lo[PSUM_START-5*OW:PSUM_START-6*OW+1], tr_data_lo[PSUM_START-6*OW:PSUM_START-7*OW+1], tr_data_lo[PSUM_START-7*OW:PSUM_START-8*OW+1]};
+    assign in_propagate = tr_data_lo[INPUT_DATA_SIZE-1];
 
     TwistMesh #(
          .N_p            (N)
@@ -144,14 +154,14 @@ module TwistMesh_tb_twotrace;
         ,.out_last_o     ( dut_ready_lo ) // Handshake r_o
     );
 
-    assign dut_data_lo[127:112] = out_psum[7];
-    assign dut_data_lo[111:96] = out_psum[6];
-    assign dut_data_lo[95:80] = out_psum[5];
-    assign dut_data_lo[79:64] = out_psum[4];
-    assign dut_data_lo[63:48] = out_psum[3];
-    assign dut_data_lo[47:32] = out_psum[2];
-    assign dut_data_lo[31:16]  = out_psum[1];
-    assign dut_data_lo[15:0]   = out_psum[0];
+    assign dut_data_lo[PSUM_START:PSUM_START-OW+1]        = out_psum[7];
+    assign dut_data_lo[PSUM_START-OW:PSUM_START-2*OW+1]   = out_psum[6];
+    assign dut_data_lo[PSUM_START-2*OW:PSUM_START-3*OW+1] = out_psum[5];
+    assign dut_data_lo[PSUM_START-3*OW:PSUM_START-4*OW+1] = out_psum[4];
+    assign dut_data_lo[PSUM_START-4*OW:PSUM_START-5*OW+1] = out_psum[3];
+    assign dut_data_lo[PSUM_START-5*OW:PSUM_START-6*OW+1] = out_psum[2];
+    assign dut_data_lo[PSUM_START-6*OW:PSUM_START-7*OW+1] = out_psum[1];
+    assign dut_data_lo[PSUM_START-7*OW:PSUM_START-8*OW+1] = out_psum[0];
 
   always_ff @(negedge clk) begin
     dut_yumi_li <= tr_ready_lo & dut_v_lo;
