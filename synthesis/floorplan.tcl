06a13d40a1bd4c4a1779a7c5791e24d9fcfe9241
hapuum <lbklbk9345@gmail.com>
2026-05-24 15:14:09 -0700

    floorplan for better hold

--- synthesis/floorplan.tcl
+++ synthesis/floorplan.tcl
@@ -1,10 +1,9 @@
-create_guide -name u_soc_top/u_sp -area {178 400 700 1785}
-create_guide -name u_soc_top/u_q -area {700 1450 1400 1785}
-create_guide -name u_soc_top/u_decoder -area {1550 1220 1785 1350}
+create_guide -name u_soc_top/u_sp -area {180 400 740 1440}
+create_guide -name u_soc_top/u_q -area {180 1400 1100 1785}
+create_guide -name u_soc_top/u_decoder -area {1500 1220 1785 1350}
 create_guide -name u_soc_top/u_disp -area {1460 1220 1550 1350}
-create_guide -name u_soc_top/u_tp -area {740 180 920 550}
-create_guide -name u_soc_top/u_mesh -area {920 180 1785 1220}
+create_guide -name u_soc_top/u_tp -area {750 180 1000 600}
+create_guide -name u_soc_top/u_mesh -area {1030 180 1785 1200}
 create_guide -name u_soc_top/u_depack -area {430 200 600 350}
 create_guide -name u_bsg_link_wrapper/link_rx_i -area {1420 1350 1785 1785}
 create_guide -name u_bsg_link_wrapper/link_tx_i -area {180 180 400 400}
-
